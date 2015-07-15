35714b1feae46de64518600b12b9b023d28217b9
Avery Anderson <avery.m.anderson@gmail.com>
2015-07-14 20:19:06 -0700

    Login and Logout urls Separate base.html and index.html

--- oysterapp/oyster/views.py
+++ oysterapp/oyster/views.py
@@ -1,7 +1,14 @@
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
 
-# Create your views here.
-class OysterView(TemplateView):
-    template_name = "index.html"
+@login_required
+def app(request):
+    return render(request, "index.html", {})
+
+
+def logout_view(request):
+    logout(request)
+    # Redirect to a success page.
