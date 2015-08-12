6acb5041d86134c6f14a447fd10b29d1b3e2df16
Avery Anderson <avery.m.anderson@gmail.com>
2015-08-11 18:50:51 -0700

    Add management command to gather all the task rules since the run of the last and create new tasks for them

---
+++ oysterapp/oyster/management/commands/update_recurring_tasks.py
@@ -0,0 +1,20 @@
+import datetime
+
+from django.core.management.base import BaseCommand
+
+from oysterapp.oyster.models import TaskRule
+
+
+class Command(BaseCommand):
+    help = "Creates tasks based off task rules"
+
+    def handle(self, *args, **options):
+        now = datetime.datetime.now()
+        last_run = TaskRule.objects.order_by('-updated').first().updated
+
+        recurring_tasks = TaskRule.objects.filter(next_scheduled_run__gt=last_run,
+                                                  next_scheduled_run__lte=now)
+
+        for task_rule in recurring_tasks:
+            task_rule.create_new_task()
+            task_rule.calculate_next_run
