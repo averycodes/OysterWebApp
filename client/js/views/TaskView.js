258e32547481adc71c21b0b09166501ef9090281
Avery Anderson <avery.m.anderson@gmail.com>
2015-09-14 19:38:59 -0700

    remove hover effects on mobile

--- client/js/views/TaskView.js
+++ client/js/views/TaskView.js
@@ -1,90 +1,94 @@
 /*global define */
 
 define([
-    'marionette',
-    'templates',
-    'underscore',
-    'app'
+  'marionette',
+  'templates',
+  'underscore',
+  'app'
 ], function (Marionette, templates, _, app) {
-    'use strict';
+  'use strict';
 
-    return Marionette.ItemView.extend({
-        className: 'task-view item',
-        template: templates.task,
+  return Marionette.ItemView.extend({
+    className: 'task-view item',
+    template: templates.task,
 
-        ui: {
-            'remove': '.remove',
-            'edit': '.edit',
-            'task': '.task',
-            'editTask': '.edit-task',
-            'newTitle': '.new-title',
-            'newAmount': '.new-amount'
-        },
+    ui: {
+      'remove': '.remove',
+      'edit': '.edit',
+      'task': '.task',
+      'editTask': '.edit-task',
+      'newTitle': '.new-title',
+      'newAmount': '.new-amount'
+    },
 
-        events: {
-            'click .task': 'onClickTask',
-            'click .remove': 'onClickRemove',
-            'click .edit': 'onClickEdit',
-            'click .save-task': 'onClickSaveTask',
-            'mouseover': 'onHover',
-            'mouseout': 'onStopHover'
-        },
+    events: {
+      'click .task': 'onClickTask',
+      'click .remove': 'onClickRemove',
+      'click .edit': 'onClickEdit',
+      'click .save-task': 'onClickSaveTask',
+      'mouseover': 'onHover',
+      'mouseout': 'onStopHover'
+    },
 
-        onRender: function() {
-            if (!this.model.get('doable')) {
-                $(this.el).addClass('disabled');
-            }
-            this.ui.remove.hide();
-            this.ui.edit.hide();
-            this.ui.editTask.hide();
-            // event listener
-        },
+    onRender: function() {
+      if (!this.model.get('doable')) {
+        $(this.el).addClass('disabled');
+      }
+      this.ui.remove.hide();
+      this.ui.edit.hide();
+      this.ui.editTask.hide();
+      // event listener
+    },
 
-        onClickTask: function(e) {
-            e.preventDefault();
-            if (this.model.get('doable')) {
-                this.model.set('completed', true);
-                this.model.save();
+    onClickTask: function(e) {
+      e.preventDefault();
+      if (this.model.get('doable')) {
+        this.model.set('completed', true);
+        this.model.save();
 
-                var user = window.app.user,
-                    bank_amt = user.get("bank");
+        var user = window.app.user,
+          bank_amt = user.get("bank");
 
-                user.set("bank", bank_amt + this.model.get("amount"));
+        user.set("bank", bank_amt + this.model.get("amount"));
 
-                this.destroy();
-            }
-        },
+        this.destroy();
+      }
+    },
 
-        onClickRemove: function(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            this.model.destroy();
-            this.destroy();
-        },
+    onClickRemove: function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      this.model.destroy();
+      this.destroy();
+    },
 
-        onClickEdit: function(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            this.ui.task.hide();
-            this.ui.editTask.show();
-        },
+    onClickEdit: function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      this.ui.task.hide();
+      this.ui.editTask.show();
+    },
 
-        onClickSaveTask: function(e) {
-            e.preventDefault();
-            this.model.set('title', this.ui.newTitle.val());
-            this.model.set('amount', this.ui.newAmount.val());
-            this.model.save();
-            this.render();
-        },
+    onClickSaveTask: function(e) {
+      e.preventDefault();
+      this.model.set('title', this.ui.newTitle.val());
+      this.model.set('amount', this.ui.newAmount.val());
+      this.model.save();
+      this.render();
+    },
 
-        onHover: function() {
-            this.ui.remove.show();
-            this.ui.edit.show();
-        },
+    onHover: function() {
+      if(!window.mobile) {
+        this.ui.remove.show();
+        this.ui.edit.show();
+      }
+    },
 
-        onStopHover: function() {
-            this.ui.remove.hide();
-            this.ui.edit.hide();
-        }
-    });
+    onStopHover: function() {
+      if (!window.mobile) {
+        this.ui.remove.hide();
+        this.ui.edit.hide();
+      }
+    }
+  });
 });
