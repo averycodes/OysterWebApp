dee71c4d643ceca20cc2a04391c9b26091731930
Avery Anderson <avery.m.anderson@gmail.com>
2015-09-14 19:18:59 -0700

    Give nav bar icons if in mobile

---
+++ client/js/views/NavView.js
@@ -0,0 +1,13 @@
+/*global define */
+
+define([
+  'marionette',
+  'templates',
+], function (Marionette, templates) {
+  'use strict';
+
+  return Marionette.ItemView.extend({
+    template: templates.nav,
+    className: 'left menu'
+  });
+});
